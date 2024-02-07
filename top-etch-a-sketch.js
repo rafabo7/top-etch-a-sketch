c85c7b89705ad018669e3f14c92ea517141105ec
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-07 17:20:42 +0100

    Refactor and add features for the grid. HTML Add an aside section for the grid size and color inputs. Refactor the createGrid() function to use it in the number input event listener => now it removes the existing grid and create a new, calculating the size of the cells and asigning it via inline style. Add the event listener for the number input. Style the main and the aside. Remove the harcoded size rules of the cells, and the border.

--- top-etch-a-sketch.js
+++ top-etch-a-sketch.js
@@ -1,20 +1,34 @@
 const container = document.querySelector('#container')
 
-function createGrid(){
-    const cell = document.createElement('div')
-    cell.classList.toggle('cell')
-    cell.addEventListener('mouseover', (e) => {
-        cell.style.backgroundColor = 'black'
-    })
-    cell.addEventListener('mouseout', (e) => {
-        cell.style.backgroundColor = 'transparent'
-        cell.style.transition = 'background-color 6s ease-in'
-    })
-    container.appendChild(cell)
+createGrid()
+
+function createGrid(grid=16){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    for (let i = 0; i < grid**2; i++){
+        const cell = document.createElement('div')
+        cell.classList.toggle('cell')
+
+
+        cellSide = 600 / grid
+        cell.style.width = `${cellSide}px`
+        cell.style.height = `${cellSide}px`
+
+        cell.addEventListener('mouseover', (e) => {
+            cell.style.backgroundColor = 'black'
+        })
+        cell.addEventListener('mouseout', (e) => {
+            cell.style.backgroundColor = 'transparent'
+            cell.style.transition = 'background-color 6s ease-in'
+        })
+        container.appendChild(cell)
+    }
 }
 
-for (let i = 0; i < 16**2; i++){
-    createGrid()
-}
+const area = document.querySelector('#grid-area')
+
+area.addEventListener('change', () => {createGrid(area.value)})
+
 
 
