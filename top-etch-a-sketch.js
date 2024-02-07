78f2a2b004de1552dd1c736e71ec4853177a32f0
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-07 11:45:37 +0100

    Add features to the game.
    Add the event listeners for the cells of the drawing area.
    Mouseover => black
    Mouseout => ease-in to white
    Manage to fill evenly the drawing area. The cells dimensions are
    calc(100% of the container / number of cell (default to 16)).
    Style the body, the header, the manins and add a border to the
    container to  make similar to an actual ecth a skect toy.

--- top-etch-a-sketch.js
+++ top-etch-a-sketch.js
@@ -3,6 +3,13 @@ const container = document.querySelector('#container')
 function createGrid(){
     const cell = document.createElement('div')
     cell.classList.toggle('cell')
+    cell.addEventListener('mouseover', (e) => {
+        cell.style.backgroundColor = 'black'
+    })
+    cell.addEventListener('mouseout', (e) => {
+        cell.style.backgroundColor = 'transparent'
+        cell.style.transition = 'background-color 6s ease-in'
+    })
     container.appendChild(cell)
 }
 
@@ -10,4 +17,4 @@ for (let i = 0; i < 16**2; i++){
     createGrid()
 }
 
-console.log('working')
+
