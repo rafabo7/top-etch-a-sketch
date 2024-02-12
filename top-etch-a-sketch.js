38010d6ddfc5576a6596c130079bd25bd5c701df
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-12 19:33:44 +0100

    Refactor features. Remove the ease in animation for the cells to return transparent, change it for static cells and a reset button, more alike to the original etch a sketch toy (you shake it to reset). Add a few comments for better readability.

--- top-etch-a-sketch.js
+++ top-etch-a-sketch.js
@@ -3,26 +3,30 @@ const container = document.querySelector('#container')
 createGrid()
 
 function createGrid(grid=64, color='#000000'){
+    // Remove the current grid
     while (container.firstChild) {
         container.removeChild(container.firstChild)
     }
-    grid = grid*(grid/2)
+    // Correct users input, odd numbers lead to bugs
+    if (grid%2 != 0){
+        grid++
+    }
+    // Create the divs to be the cells of the grid
+    grid = grid*(grid/2) // Formula to fit the aspect ratio of the grid
     for (let i = 0; i < grid; i++){
         const cell = document.createElement('div')
         cell.classList.toggle('cell')
+        container.appendChild(cell)
 
+        // Formula to get the size of the cell
         cellSide = Math.sqrt((600*1200)/(grid))
         cell.style.width = `${cellSide}px`
         cell.style.height = `${cellSide}px`
 
+        // Event listener to change color when mouse pass over
         cell.addEventListener('mouseover', (e) => {
             cell.style.backgroundColor = color
         })
-        cell.addEventListener('mouseout', (e) => {
-            cell.style.backgroundColor = 'transparent'
-            cell.style.transition = 'background-color 6s ease-in'
-        })
-        container.appendChild(cell)
     }
 }
 
@@ -33,6 +37,10 @@ area.addEventListener('change', () => {createGrid(area.value, color.value)})
 const color = document.querySelector('#color')
 
 color.addEventListener('change', () => {createGrid(area.value, color.value)})
+
+const resetBtn = document.querySelector('#reset')
+
+resetBtn.addEventListener('click', () => {createGrid(area.value, color.value)})
 
 
 
